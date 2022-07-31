b24db8ba581fe5993e0d6b8453f4c84013397da2
Santanu Biswas <69161246+Santanu29@users.noreply.github.com>
2022-07-31 16:56:46 +0530

    Create 2357. Make Array Zero by Subtracting Equal Amounts.cpp

---
+++ C++/2357. Make Array Zero by Subtracting Equal Amounts.cpp
@@ -0,0 +1,47 @@
+/*
+You are given a non-negative integer array nums. In one operation, you must:
+
+Choose a positive integer x such that x is less than or equal to the smallest non-zero element in nums.
+Subtract x from every positive element in nums.
+Return the minimum number of operations to make every element in nums equal to 0.
+
+ 
+
+Example 1:
+
+Input: nums = [1,5,0,3,5]
+Output: 3
+Explanation:
+In the first operation, choose x = 1. Now, nums = [0,4,0,2,4].
+In the second operation, choose x = 2. Now, nums = [0,2,0,0,2].
+In the third operation, choose x = 2. Now, nums = [0,0,0,0,0].
+Example 2:
+
+Input: nums = [0]
+Output: 0
+Explanation: Each element in nums is already 0 so no operations are needed.
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+0 <= nums[i] <= 100
+*/
+
+
+/*
+    NOTE:
+    We always decrease the numbers by the least number in the array.
+    The answer here becomes the number number of unique elements in the array because the number of operations is 1 for same elements.
+
+    Look at the hint for more info.
+*/
+
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        unordered_set<int> s(nums.begin(), nums.end());
+        
+        return s.size() - (s.find(0)==s.end() ? 0 : 1);
+    }
+};
