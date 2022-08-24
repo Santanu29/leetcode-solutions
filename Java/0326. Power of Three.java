d8e6cddb31af5f66dd580760c7c34da969322aaa
Santanu Biswas <69161246+Santanu29@users.noreply.github.com>
2022-08-24 10:55:12 +0530

    Create 0326. Power of Three.java

---
+++ Java/0326. Power of Three.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if(n<1) return false;
+        if(n==1)    return true;
+        
+        while(n>1)
+        {
+            if(n%3==0)
+                n=n/3;
+            else
+                return false;
+        }
+        return true;
+    }
+}
